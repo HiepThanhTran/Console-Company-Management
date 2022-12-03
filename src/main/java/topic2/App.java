925833e383cba0b1c36bf0b077902b75b0becd3a
Maryokuu <hieptt.2003@gmail.com>
2022-12-03 17:29:00 +0700

    init

--- src/main/java/topic2/App.java
+++ src/main/java/topic2/App.java
@@ -2,13 +2,14 @@ package topic2;
 
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+import topic2.view.MouseExampleView;
 
 public class App {
 
     public static void main(String[] args) {
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-            
+            new MouseExampleView();
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         } catch (InstantiationException e) {
