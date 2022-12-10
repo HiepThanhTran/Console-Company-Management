d0ba970ffbd9253e767c8341c1b6db95fcf07646
Maryokuu <hieptt.2003@gmail.com>
2022-12-10 20:42:09 +0700

    init

--- src/main/java/topic2/color/Color.java
+++ src/main/java/topic2/color/Color.java
@@ -3,7 +3,6 @@ package topic2.color;
 public enum Color {
     RESET("\033[0m"),
 
-    // Regular Colors. Normal color, no bold, background color etc.
     BLACK("\033[0;30m"),
     RED("\033[0;31m"),
     GREEN("\033[0;32m"),
